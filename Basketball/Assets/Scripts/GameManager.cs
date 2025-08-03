e044e415092a5f5111b6c41eb3dab7928e5a4178
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2025-08-03 20:21:25 +0700

    add high score function and activate particles when scoring

--- Basketball/Assets/Scripts/GameManager.cs
+++ Basketball/Assets/Scripts/GameManager.cs
@@ -8,15 +8,46 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public Text currentScoreText;
     private int currentScore = 0;
+    public Text highScoreText;
+    private int highScore = 0;
+    public Text boardText;
+
+    public ParticleSystem perfectParticle;
+    public ParticleSystem fireParticle;
 
     private void Awake()
     {
         instance = this;
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        UpdateUI();
     }
 
     public void UpdateScore()
     {
         currentScore++;
+        if (currentScore > highScore)
+        {
+            highScore = currentScore;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+            fireParticle.Play();
+        }
+        UpdateUI();
+        perfectParticle.Play();
+        StartCoroutine(AppearText("GREAT SHOT"));
+    }
+
+    private void UpdateUI()
+    {
         currentScoreText.text = currentScore.ToString();
+        highScoreText.text = highScore.ToString();
+    }
+
+    private IEnumerator AppearText(string text)
+    {
+        boardText.text = text;
+        boardText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        boardText.gameObject.SetActive(false);
     }
 }
